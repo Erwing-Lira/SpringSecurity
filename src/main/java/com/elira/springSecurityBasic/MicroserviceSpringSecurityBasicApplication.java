485688ef69a73f921b6ab6b7af7c1929ca83f5ee
Erwing-Lira <erwing_fox@hotmail.com>
2024-11-26 19:04:30 -0600

    First commit

---
+++ src/main/java/com/elira/springSecurityBasic/MicroserviceSpringSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.elira.springSecurityBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringSecurityBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringSecurityBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elira/springSecurityBasic/MicroserviceSpringSecurityBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elira.springSecurityBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringSecurityBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
